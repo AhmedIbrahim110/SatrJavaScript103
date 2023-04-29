3c8cdf8f470872a56dedcba1e82cdeb7bb6914a3
DerRiese <DerRiese765@gmail.com>
2023-04-29 08:34:01 +0300

    -

---
+++ main.js
@@ -0,0 +1,16 @@
+/*const myString = 'I love JavaScript'
+const regex = /JavaScript/
+const containsRegex = regex.test(myString)
+console.log(containsRegex)
+
+const myString = 'I love JavaScript'
+const myRegex = /JavaScript/
+let newString = myString.replace(myRegex, 'Kotlin')
+console.log(newString);
+
+
+const myString = 'I love JavaScript'
+let newRegex = myString.search(/JavaScript/)
+console.log(newRegex);
+
+*/
